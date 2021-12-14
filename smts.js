44509d53309012263bf8725238011b67af490719
lososii <93191109+lososii@users.noreply.github.com>
2021-12-14 11:57:23 +0200

    Update smts.js

--- smts.js
+++ smts.js
@@ -21,7 +21,7 @@ $(function()
             $('#error_message').show();
 
             //reverse the response on the button
-            $('button[type="button"]', $form).each(function()
+            $('button[type="submit"]', $form).each(function()
             {
                 $btn = $(this);
                 label = $btn.prop('orig_label');
